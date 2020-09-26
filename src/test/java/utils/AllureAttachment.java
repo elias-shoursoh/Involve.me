3694afcf86f4e65f9dc0d1b6f21a067da05d6ea2
Elias Shourosh <eliass@flashnetworks.com>
2020-09-26 15:07:44 +0300

    add attachment page source to allure

--- src/test/java/utils/AllureAttachment.java
+++ src/test/java/utils/AllureAttachment.java
@@ -1,5 +1,11 @@
 package utils;
 
+import java.nio.charset.StandardCharsets;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import io.qameta.allure.Attachment;
 
 public class AllureAttachment {
@@ -7,6 +13,19 @@ public class AllureAttachment {
 	@Attachment(value = "{0}", type = "text/plain")
 	public static String addTextAttachment(String message) {
 		return message;
+	}
+	
+	
+	@Attachment(value = "Page Screenshot", type = "image/png", fileExtension = ".png")
+	public static byte[] captureScreenshot(WebDriver driver) {
+		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+	}
+	
+	
+
+	@Attachment(value = "Html source", type = "text/html", fileExtension = ".html")
+	public static byte[] getPageSource(WebDriver driver) {
+		return driver.getPageSource().getBytes(StandardCharsets.UTF_8);
 	}
 
 }
